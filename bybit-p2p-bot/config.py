c17956fc59a868f8e7c24cad916d66097274d1a1
akinrinadeakinniyi401-dot <akinrinadeakinniyi401@gmail.com>
2026-04-02 16:33:45 +0100

    Update config.py

--- bybit-p2p-bot/config.py
+++ bybit-p2p-bot/config.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+BYBIT_API_KEY = os.getenv("BYBIT_API_KEY")
+BYBIT_API_SECRET = os.getenv("BYBIT_API_SECRET")
+ADMIN_ID = int(os.getenv("ADMIN_TELEGRAM_ID"))
