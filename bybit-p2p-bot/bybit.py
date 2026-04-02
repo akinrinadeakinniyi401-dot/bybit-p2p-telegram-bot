456d559bd2aec8eb3a7268c9bc0643c04dd34ae6
akinrinadeakinniyi401-dot <akinrinadeakinniyi401@gmail.com>
2026-04-02 16:36:49 +0100

    Update bybit.py

--- bybit-p2p-bot/bybit.py
+++ bybit-p2p-bot/bybit.py
@@ -1,1 +1,19 @@
+import time
+import hmac
+import hashlib
+import requests
+import json
+from config import BYBIT_API_KEY, BYBIT_API_SECRET
 
+BASE_URL = "https://api.bybit.com"
+
+def generate_signature(payload: str, timestamp: str, recv_window="5000"):
+    param_str = f"{timestamp}{BYBIT_API_KEY}{recv_window}{payload}"
+    
+    signature = hmac.new(
+        bytes(BYBIT_API_SECRET, "utf-8"),
+        param_str.encode("utf-8"),
+        hashlib.sha256
+    ).hexdigest()
+
+    return signature
