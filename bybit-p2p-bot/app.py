acd319cf5c76adf0ada8a264dd2f8ca4395ef972
akinrinadeakinniyi401-dot <akinrinadeakinniyi401@gmail.com>
2026-04-02 16:34:09 +0100

    Update app.py

--- bybit-p2p-bot/app.py
+++ bybit-p2p-bot/app.py
@@ -1,1 +1,20 @@
+from flask import Flask
+import requests
+from bot import start_bot
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+if __name__ == "__main__":
+    # 🌍 Print Render IP
+    try:
+        ip = requests.get("https://api.ipify.org").text
+        print(f"🌍 Render Public IP: {ip}")
+    except:
+        print("IP fetch failed")
+
+    start_bot()
+    app.run(host="0.0.0.0", port=3000)
